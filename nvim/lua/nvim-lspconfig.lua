997dfdb8f59bf37fb3e226e27d249e848ee048d0
Peter McEvoy <git@mcevoypeter.com>
2024-08-01 08:12:27 -0400

    Add Terraform lang server

--- nvim/lua/nvim-lspconfig.lua
+++ nvim/lua/nvim-lspconfig.lua
@@ -3,4 +3,8 @@
 local lspconfig = require("lspconfig")
 vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 
+-- brew install hashicorp/tap/terraform-ls
+lspconfig.terraformls.setup{}
+
+-- npm i -g typescript typescript-language-server
 lspconfig.tsserver.setup{}
