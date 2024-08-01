f6a98e527f77f273caab4f474ffd9954f1012b9b
Peter McEvoy <git@mcevoypeter.com>
2024-08-01 07:50:47 -0400

    Map Shift+K to LSP hover

--- nvim/lua/nvim-lspconfig.lua
+++ nvim/lua/nvim-lspconfig.lua
@@ -1,5 +1,6 @@
 -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
 local lspconfig = require("lspconfig")
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 
 lspconfig.tsserver.setup{}
