0b54d80cd71782732f3b8b66bdd04107f60ab82c
Peter McEvoy <git@mcevoypeter.com>
2024-10-25 13:41:12 -0400

    Autoreload buffer when file changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,4 +99,16 @@ vim.cmd([[
   nnoremap <Leader>l :copen<CR>
 ]])
 
+-- Auto-reload files when modified externally. See
+-- https://neovim.discourse.group/t/a-lua-based-auto-refresh-buffers-when-they-change-on-disk-function/2482
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
+  command = "echohl WarningMsg | echo \"File changed on disk. Buffer reloaded.\" | echohl None",
+  pattern = { "*" },
+})
+
 require("config.lazy")
