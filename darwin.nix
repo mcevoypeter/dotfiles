bad32bf53c2861459f1d6ead16e7b6a6cd9cbfb6
Peter McEvoy <git@mcevoypeter.com>
2026-06-02 12:40:25 -0400

    Add unfree predicate for op

--- darwin.nix
+++ darwin.nix
@@ -1,7 +1,11 @@
-{ ... }: {
+{ lib, ... }: {
   imports = [ ./homebrew ];
 
   nixpkgs.hostPlatform = "aarch64-darwin";
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "1password-cli"
+    ];
   system.stateVersion = 5;
   users.users.peter = {
     name = "peter";
