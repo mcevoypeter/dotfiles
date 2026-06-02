65abc437158f53cbbaffccd8376f686271f4e4f0
Peter McEvoy <git@mcevoypeter.com>
2026-06-02 13:05:06 -0400

    Disable auto migrate for nix-managed brew

--- homebrew/default.nix
+++ homebrew/default.nix
@@ -2,9 +2,6 @@
   nix-homebrew = {
     enable = true;
     user = "peter";
-
-    # An existing Homebrew install lives at /usr/local. Let nix-homebrew
-    # take it over on first activation, keeping already-installed packages.
-    autoMigrate = true;
+    autoMigrate = false;
   };
 }
